97517a7ec95ae7549c96b239e3a68d753cfb3bfa
bellasatpathyhorton <ghorton2@jh.edu>
2025-02-04 14:54:51 -0500

    started helper functions for test that

---
+++ tests/testthat/test-mle-finder.R
@@ -0,0 +1,19 @@
+test_that("linalg and optm least-sq coincide", {
+          # simulate data
+          n_obs <- 32
+          n_pred <- 4
+          data <- simulate_data(n_obs, n_pred, seed=1918)
+          outcome <- data$outcome
+          design <- data$design
+          # get MLE using analytical formula
+          linalg_out <- hiper_glm(design, outcome, moddel = "linear")
+          # get MLE using stats::optim
+          optim_out <- hiper_glm(design, outcome, moddel = "linear", option = list(mle_finder = "BFGS"))
+          # compare the two
+          expect_true(
+            are_all_close(coef(linalg_out), coef(optim_out))
+          )
+          })
+
+
+# devtools::test()
